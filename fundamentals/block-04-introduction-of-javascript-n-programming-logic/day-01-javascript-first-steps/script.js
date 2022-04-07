bb189143c43dfa26d75247844b146a8a35552081
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-04-07 11:08:43 -0300

    Second requirement

--- fundamentals/block-04-introduction-of-javascript-n-programming-logic/day-01-javascript-first-steps/script.js
+++ fundamentals/block-04-introduction-of-javascript-n-programming-logic/day-01-javascript-first-steps/script.js
@@ -5,3 +5,14 @@ console.log(75 - 25);
 console.log(25 * 45);
 console.log(785 / 4);
 console.log(955 % 6);
+
+//2
+
+const a = 25;
+const b = 35;
+
+if (a > b) {
+    console.log(a);
+} else {
+    console.log(b);
+}
