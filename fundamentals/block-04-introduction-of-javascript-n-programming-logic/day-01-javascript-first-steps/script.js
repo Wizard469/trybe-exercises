8c31bd6f03c36a9b0a53aebbc262b5c2013262c1
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-04-07 18:49:46 -0300

    Eighth requirement

--- fundamentals/block-04-introduction-of-javascript-n-programming-logic/day-01-javascript-first-steps/script.js
+++ fundamentals/block-04-introduction-of-javascript-n-programming-logic/day-01-javascript-first-steps/script.js
@@ -103,3 +103,14 @@ if (givenNote >= 90) {
     console.log('Erro, nota incorreta')
 }
 
+//8
+
+const number1 = 10;
+const number2 = 15;
+const number3 = 49;
+
+if (number1 % 2 === 0 || number2 % 2 === 0 || number3 % 2 === 0) {
+    console.log(true)
+} else {
+    console.log(false)
+}
