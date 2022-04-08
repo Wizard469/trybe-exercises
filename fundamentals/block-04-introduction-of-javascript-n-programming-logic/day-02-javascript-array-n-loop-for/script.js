4b21dbef1772db17d1df502134880490b9e23bff
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-04-08 10:48:04 -0300

    Second requirement

--- fundamentals/block-04-introduction-of-javascript-n-programming-logic/day-02-javascript-array-n-loop-for/script.js
+++ fundamentals/block-04-introduction-of-javascript-n-programming-logic/day-02-javascript-array-n-loop-for/script.js
@@ -5,3 +5,13 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let i = 0; i < numbers.length; i += 1) {
     console.log(numbers[i]);
 }
+
+//2.
+
+ let sumOfNumbers = 0;
+
+for (let i = 0; i < numbers.length; i += 1) {
+        sumOfNumbers += numbers[i];
+}
+
+console.log(sumOfNumbers);
