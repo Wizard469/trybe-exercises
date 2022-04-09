9a457a4df43a630c1ccef208b94e71e38df0686a
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-04-09 10:20:05 -0300

    Second requirement

--- fundamentals/block-04-introduction-of-javascript-n-programming-logic/day-03-javascript-programming-logic-n-algorithm/script.js
+++ fundamentals/block-04-introduction-of-javascript-n-programming-logic/day-03-javascript-programming-logic-n-algorithm/script.js
@@ -1,9 +1,20 @@
 //1
 
-let factorial = 10;
+let factorial = 1;
 
-for (let i = 1; i < 10; i += 1) {
+for (let i = 10; i > 0; i -= 1) {
     factorial = factorial * i;
 }
 
 console.log(factorial);
+
+//2
+
+let word = 'arroz';
+let reverse = '';
+
+for (let i = word.length - 1; i >= 0; i -= 1) {
+    reverse = reverse + word[i];
+}
+
+console.log(reverse);
