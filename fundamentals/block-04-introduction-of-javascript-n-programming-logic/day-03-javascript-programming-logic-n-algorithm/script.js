947f64e96a5324c3945accb4a87bc0b86a1112ce
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-04-09 11:52:35 -0300

    Third requirement

--- fundamentals/block-04-introduction-of-javascript-n-programming-logic/day-03-javascript-programming-logic-n-algorithm/script.js
+++ fundamentals/block-04-introduction-of-javascript-n-programming-logic/day-03-javascript-programming-logic-n-algorithm/script.js
@@ -18,3 +18,26 @@ for (let i = word.length - 1; i >= 0; i -= 1) {
 }
 
 console.log(reverse);
+
+//3
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let longestString = array[0];
+let shortestString = array[0];
+
+for (let i = 0; i < array.length; i += 1) {
+    if (array[i].length > longestString.length) {
+        longestString = array[i];
+    }
+}
+
+console.log(longestString);
+
+for (let i = 0; i < array.length; i += 1) {
+    if (array[i].length < shortestString.length) {
+        shortestString = array[i];
+    }
+}
+
+console.log(shortestString)
