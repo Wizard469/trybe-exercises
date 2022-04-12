882830c3b0ca422629db70617af5c781622d2a90
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-04-12 10:13:46 -0300

    Part1 second requirement

--- fundamentals/block-04-introduction-of-javascript-n-programming-logic/day-04-javascript-objects-n-functions/part1.js
+++ fundamentals/block-04-introduction-of-javascript-n-programming-logic/day-04-javascript-objects-n-functions/part1.js
@@ -6,4 +6,9 @@ let info = {
 
   //1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
 
-  console.log("Boas vindas, " + info.personagem)
+  console.log("Boas vindas, " + info.personagem);
+
+  //2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
+
+  info.recorrente = 'Sim';
+  console.log(info);
