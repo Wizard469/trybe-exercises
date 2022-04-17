72772ca1e9e3ee16ac5ec5f866bf087c0d7833a5
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-04-17 14:38:02 -0300

    Part2 second requirement

--- fundamentals/block-04-introduction-of-javascript-n-programming-logic/day-04-javascript-objects-n-functions/part2.js
+++ fundamentals/block-04-introduction-of-javascript-n-programming-logic/day-04-javascript-objects-n-functions/part2.js
@@ -1,4 +1,4 @@
-//1 - Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
+//1 - Crie uma função que receba uma string e retorne true se for um palíndromo, ou false, se não for.
     
 
 function palindromeVerification(string) {
@@ -12,3 +12,17 @@ function palindromeVerification(string) {
    }
 }
 console.log(palindromeVerification("caro"));
+
+//2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+
+function largerIntegerIndex(numbers) {    
+    let largerIndex = 0;
+
+    for (let index in numbers) {
+        if (largerIndex < numbers[index]) {
+            largerIndex = index;
+        }
+    }
+    return largerIndex;
+}
+console.log(largerIntegerIndex([2, 3, 6, 7, 10, 1]));
