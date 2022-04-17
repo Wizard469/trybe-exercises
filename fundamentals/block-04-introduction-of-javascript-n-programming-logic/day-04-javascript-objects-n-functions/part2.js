dec55b4fb2bfd9d3b63281e139d2b1bc431797a9
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-04-17 16:06:54 -0300

    Part2 third requirement

--- fundamentals/block-04-introduction-of-javascript-n-programming-logic/day-04-javascript-objects-n-functions/part2.js
+++ fundamentals/block-04-introduction-of-javascript-n-programming-logic/day-04-javascript-objects-n-functions/part2.js
@@ -26,3 +26,17 @@ function largerIntegerIndex(numbers) {
     return largerIndex;
 }
 console.log(largerIntegerIndex([2, 3, 6, 7, 10, 1]));
+
+//3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+
+function smallerNumberIndex(numbers) {
+    let smallerNumber = numbers[0];
+
+    for (let i in numbers) {
+        if (numbers[smallerNumber] > numbers[i]) {
+            smallerNumber = i;
+        }
+    }
+    return smallerNumber;
+}
+console.log(smallerNumberIndex([2, 4, 6, 7, 10, 0, -3]));
