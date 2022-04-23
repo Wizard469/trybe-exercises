f13ecdfc494b68b76d7b2252bc67686083a091da
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-04-23 18:32:08 -0300

    Actual eighth requirement :)

--- fundamentals/block-05-javascript-dom-events-n-web-storage/day-03-javascript-events/script.js
+++ fundamentals/block-05-javascript-dom-events-n-web-storage/day-03-javascript-events/script.js
@@ -167,3 +167,18 @@ function newTask(task) {
 }
 
 newTask("Walk the dog!");
+
+/* Exercício 8:
+Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task.
+O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks". */
+
+function newTaskDiv(color) {
+  let myTasks = document.querySelector('.my-tasks');
+  let newTask = document.createElement('div');
+
+  newTask.style.backgroundColor = color;
+  myTasks.appendChild(newTask);
+}
+
+newTaskDiv("red");
