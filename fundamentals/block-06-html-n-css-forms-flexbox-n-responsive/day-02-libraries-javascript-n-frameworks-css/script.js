381899817bbcfe4fe8289e7ac9d39dbb561ae491
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-04-30 10:43:00 -0300

    Inicial commit

---
+++ fundamentals/block-06-html-n-css-forms-flexbox-n-responsive/day-02-libraries-javascript-n-frameworks-css/script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+
+    const body = document.body;
+
+    let sendButton = document.querySelector('.buttons');
+
+    sendButton.addEventListener('click', function(event) {
+        event.preventDefault();
+    })
+}
