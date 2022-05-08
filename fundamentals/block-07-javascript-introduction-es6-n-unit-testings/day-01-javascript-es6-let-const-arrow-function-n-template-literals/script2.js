ecbd70609962ed0e69c491ece023866d23c3f5d0
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-05-08 20:21:20 -0300

    Part-2 first exercise

---
+++ fundamentals/block-07-javascript-introduction-es6-n-unit-testings/day-01-javascript-es6-let-const-arrow-function-n-template-literals/script2.js
@@ -0,0 +1,24 @@
+// Abaixo, você verá algumas especificações de algoritmos para desenvolver. É fundamental que você utilize o que aprendeu sobre let, const, arrow functions, template literals e ternary operator.
+
+//1. Crie uma função que receba um número e retorne seu fatorial.
+
+// Na matemática, o fatorial de um número não negativo N, com a notação N!, é o produto de todos os inteiros menores ou iguais a N. Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
+// Bônus (opcional): tente fazer o mesmo exercício de forma recursiva. Spoiler: É possível resolver com uma linha usando ternary operator.
+
+const factorial = (N) => {
+    let result = 1;
+    if (N === 0) {
+        return 1;
+    } else {
+        for (let index = 2; index <= N; index += 1) {
+            result = result *= index;
+        }
+        return result;
+    }
+}
+
+console.log(factorial(10));
+
+// Recursiva
+// const factorials = (N) => N > 1 ? N * factorials(N - 1) : 1;
+// console.log(factorials(5));
