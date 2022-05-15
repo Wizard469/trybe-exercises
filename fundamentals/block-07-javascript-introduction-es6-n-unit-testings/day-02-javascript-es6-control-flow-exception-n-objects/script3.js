2773c7a328841b6df81c783768b700d973ba7a8e
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-05-15 16:01:17 -0300

    Part-3 second requirement

--- fundamentals/block-07-javascript-introduction-es6-n-unit-testings/day-02-javascript-es6-control-flow-exception-n-objects/script3.js
+++ fundamentals/block-07-javascript-introduction-es6-n-unit-testings/day-02-javascript-es6-control-flow-exception-n-objects/script3.js
@@ -18,6 +18,15 @@ const lesson1 = {
     turno: 'noite',
   };
 
+  //1. Crie uma função para adicionar o turno da noite na lesson2. Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+
   const addingUpKey = (obj, key, value) => obj[key] = value;
 
   addingUpKey(lesson2, "turno", "noite");
+
+  //2. Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+
+  const objKeysList = (obj) => Object.keys(obj);
+
+  objKeysList(lesson3);
+  
