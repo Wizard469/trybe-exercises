df84918dfcedd4d18a3a39b06d0fa67b3245bf1b
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-05-15 15:26:41 -0300

    Part-2 second requirement

--- fundamentals/block-07-javascript-introduction-es6-n-unit-testings/day-02-javascript-es6-control-flow-exception-n-objects/script2.js
+++ fundamentals/block-07-javascript-introduction-es6-n-unit-testings/day-02-javascript-es6-control-flow-exception-n-objects/script2.js
@@ -33,19 +33,26 @@ const order = {
       total: 60,
     },
   };
-  //${}
+
   const customerInfo = (order) => {
-    return `Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R: ${order.address.street}, Nº: ${order.address.number}, AP: ${order.address.apartment}`
-  }
-  
+    return `Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R: ${order.address.street}, Nº: ${order.address.number}, AP: ${order.address.apartment}`;
+  };
+
   customerInfo(order);
-  
+
   const orderModifier = (order) => {
-    // Adicione abaixo as informações necessárias.
-  
-  }
+    const newCustomer = order.name = "Luiz Silva";
+    const total = order.payment.total = '50';
+    const pizzas = Object.keys(order.order.pizza);
+
+    return `Olá ${newCustomer}, o total do seu pedido de ${pizzas[0]}, ${pizzas[1]} e ${order.order.drinks.coke.type} é R$ ${total},00.`;
+  };
   
   orderModifier(order);
 
-  //1. Complete a função customerInfo() para que seu retorno seja similar a "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
+//1. Complete a função customerInfo() para que seu retorno seja similar a "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
 //Note que o parâmetro da função já está sendo passado na chamada da função.
+
+//2. Complete a função orderModifier() para que seu retorno seja similar a "Olá Luiz Silva, o total do seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00."
+//Modifique o nome da pessoa compradora.
+//Modifique o valor total da compra para R$ 50,00.
