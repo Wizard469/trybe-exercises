5f621322ced1e6cf27f00fd3bf275d3060c76752
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-05-17 11:29:46 -0300

    Third requirement

--- fundamentals/block-08-higher-order-functions-of-javascript-es6/day-01-javascript-es6-higher-order-functions-introduction/script.js
+++ fundamentals/block-08-higher-order-functions-of-javascript-es6/day-01-javascript-es6-higher-order-functions-introduction/script.js
@@ -26,4 +26,31 @@ const lotteryResult = (myNumber, callback) => {
     return callback(myNumber, sweepTake) ? "Parabéns você ganhou" : "Tente novamente";
 }
 
-console.log(lotteryResult(3, compareNumbers));
+// console.log(lotteryResult(3, compareNumbers));
+
+// 3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas do(a) estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pelo(a) estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+
+// Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const compareAnswers = (corretAnswers, studentAnswers) => {
+    if (corretAnswers === studentAnswers) {
+        return 1;
+    } if (studentAnswers === "N.A") {
+        return 0;
+    }
+    return -0.5;
+}
+
+const ratingCount = (corretAnswers, studentAnswers, callback) => {
+    let counter = 0;
+    
+    for (let index = 0; index < corretAnswers.length; index += 1) {
+        counter += callback(corretAnswers[index], studentAnswers[index]);
+    }
+    return `Resultado final: ${counter} pontos`;
+};
+
+// console.log(ratingCount(RIGHT_ANSWERS, STUDENT_ANSWERS, compareAnswers));
