26bcc1e4def6802e315411b734b772f5a3b1e1a6
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-05-18 19:20:54 -0300

    Second exercise

--- fundamentals/block-08-higher-order-functions-of-javascript-es6/day-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise2.js
+++ fundamentals/block-08-higher-order-functions-of-javascript-es6/day-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise2.js
@@ -70,7 +70,13 @@ const books = [
 function smallerName() {
   let nameBook;
   // escreva aqui o seu código
-
+  books.forEach((book) => {
+    if (!nameBook || book.name.length < nameBook.length) {
+      nameBook = book.name;
+    }
+  });
   // Variável nameBook que receberá o valor do menor nome;
   return nameBook;
 }
+
+console.log(smallerName());
