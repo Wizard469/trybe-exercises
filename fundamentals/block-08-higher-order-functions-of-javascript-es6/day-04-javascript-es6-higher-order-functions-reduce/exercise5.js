71c292ec9cc35a9c8339429fd9bdb0464d2eb889
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-05-27 16:58:01 -0300

    Inicial commit

---
+++ fundamentals/block-08-higher-order-functions-of-javascript-es6/day-04-javascript-es6-higher-order-functions-reduce/exercise5.js
@@ -0,0 +1,11 @@
+// 🚀 5- Dado o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúsculo ou minúsculo.
+
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+function containsA() {
+  // escreva seu código aqui
+}
