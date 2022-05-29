5f8a3c68f1babdfee0b9278ff271f5bcdd8066c0
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-05-29 14:02:24 -0300

    Seventh exercise

--- fundamentals/block-08-higher-order-functions-of-javascript-es6/day-05-javascript-es6-spread-operator-rest-parameter-destructuring-n-more/exercise7.js
+++ fundamentals/block-08-higher-order-functions-of-javascript-es6/day-05-javascript-es6-spread-operator-rest-parameter-destructuring-n-more/exercise7.js
@@ -22,6 +22,8 @@ const ships = [
 
 // escreva shipLength abaixo
 
-console.log(shipLength(ships[0])); // 'Titanic is 269.1 meters long'
-console.log(shipLength(ships[1])); // 'Queen Mary 2 is 1132 feet long'
-console.log(shipLength(ships[2])); // 'Yamato is 256 meters long'
+const shipLength = ({ name, length, measurementUnit }) => `${name} is ${length} ${measurementUnit} long.` ;
+
+console.log(shipLength(ships[0])); // 'Titanic is 269.1 meters long.'
+console.log(shipLength(ships[1])); // 'Queen Mary 2 is 1132 feet long.'
+console.log(shipLength(ships[2])); // 'Yamato is 256 meters long.'
