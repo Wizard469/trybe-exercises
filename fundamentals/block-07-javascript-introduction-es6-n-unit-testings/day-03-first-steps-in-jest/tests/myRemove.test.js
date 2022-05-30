9271ed6e5ac266876e0f128f8ed74c7bf7659028
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-05-30 20:25:59 -0300

    Second exercise

--- fundamentals/block-07-javascript-introduction-es6-n-unit-testings/day-03-first-steps-in-jest/tests/myRemove.test.js
+++ fundamentals/block-07-javascript-introduction-es6-n-unit-testings/day-03-first-steps-in-jest/tests/myRemove.test.js
@@ -1,1 +1,9 @@
-const hydrate = require('../src/hydrate.js');
+const myRemove = require('../src/myRemove.js');
+
+describe('Test the function hydrate', () => {
+  it('should receive the expected return', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+});
