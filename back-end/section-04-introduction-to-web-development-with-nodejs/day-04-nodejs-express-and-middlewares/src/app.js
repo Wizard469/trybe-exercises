127ccc67d1b2e9fe7505d209ff240db65baff48f
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-10-04 10:37:03 -0300

    Exercise 1.5

--- back-end/section-04-introduction-to-web-development-with-nodejs/day-04-nodejs-express-and-middlewares/src/app.js
+++ back-end/section-04-introduction-to-web-development-with-nodejs/day-04-nodejs-express-and-middlewares/src/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 const validateName = require('./middlewares/validateName');
+const validatePrice = require('./middlewares/validatePrice');
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/activities', validateName, (_req, res) => {
+app.post('/activities', validateName, validatePrice, (_req, res) => {
   res.status(201).json({ "message": "Atividade cadastrada com sucesso!" });
 });
 
